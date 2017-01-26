85c4c9ada03f97112ace91ea47b95bb9868e76bb
Stephane Liu <sliu@sjliu.com>
2017-01-25 21:35:34 -1000

    Mod install to be rcm compat

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+set -e
+
+is_osx(){
+  [ "$(uname -s)" = Darwin ]
+}
+  
+is_linux(){
+  [ "$(uname -s)" = Linux ]
+}
+
+if is_osx; then
+  echo "Installing Homebrew packages..."
+  brew update
+  brew tap homebrew/bundle
+  brew bundle
+  for brewfile in */Brewfile; do
+    brew bundle --file="$brewfile"
+  done
+fi
+
+echo "Linking dotfiles into ~..."
+# Before `rcup` runs, there is no ~/.rcrc, so we must tell `rcup` where to look.
+# We need the rcrc because it tells `rcup` to ignore thousands of useless Vim
+# backup files that slow it down significantly.
+RCRC=rcrc rcup -v -d .
+
+echo "Installing Vim packages..."
+vim +PlugInstall +qa
+
+if is_osx; then
+  echo
+  echo "If you like what you see in system/osx-settings, run ./system/osx-settings"
+  echo "If you're using Terminal.app, check out the terminal-themes directory"
+fi
+
+for setup in tag-*/setup; do
+  . "$setup"
+done
