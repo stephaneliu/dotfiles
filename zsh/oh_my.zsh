c46e82b8350a7809c529e3ca7f3a455034bad8f8
Stephane Liu <sliu@sjliu.com>
2017-01-27 17:01:05 -1000

    Use `z` instead of `j` for directory

--- zsh/oh_my.zsh
+++ zsh/oh_my.zsh
@@ -4,7 +4,7 @@ ZSH_THEME="sliu"
 COMPLETION_WAITING_DOTS="true"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-plugins=(autojump colored-man bundler code-dir command-not-found history-substring-search vi-mode web-search z chucknorris)
+plugins=(colored-man code-dir command-not-found history-substring-search vi-mode web-search z chucknorris)
 
 source $ZSH/oh-my-zsh.sh
 
