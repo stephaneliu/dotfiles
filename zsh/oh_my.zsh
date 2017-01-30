0de4738d74e8ee8a3cfa3bceb5993a717f79114d
Stephane Liu <sliu@sjliu.com>
2017-01-30 11:50:06 -1000

    Rearrange activation of chuck_cow

--- zsh/oh_my.zsh
+++ zsh/oh_my.zsh
@@ -1,3 +1,5 @@
+is_linux() { [ "$(uname -s)" = Linux ] }
+
 ZSH=$HOME/.oh-my-zsh
 ZSH_THEME="robbyrussell"
 
@@ -6,11 +8,13 @@ COMPLETION_WAITING_DOTS="true"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 plugins=(colored-man code-dir command-not-found history-substring-search vi-mode web-search z)
 
-source $ZSH/oh-my-zsh.sh
-
-is_linux() { [ "$(uname -s)" = Linux ] }
-
 if is_linux; then
   plugins+=chucknorris
+fi
+  
+source $ZSH/oh-my-zsh.sh
+
+if is_linux; then
   chuck_cow
 fi
+
