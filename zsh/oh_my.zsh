5ecbdca812eb2d0c60d295982623fe4e5daa5956
Stephane Liu <sliu@sjliu.com>
2017-02-01 12:19:24 -1000

    Add autojump to zsh plugin

--- zsh/oh_my.zsh
+++ zsh/oh_my.zsh
@@ -6,7 +6,7 @@ ZSH_THEME="robbyrussell"
 COMPLETION_WAITING_DOTS="true"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-plugins=(colored-man code-dir command-not-found history-substring-search vi-mode web-search z)
+plugins=(colored-man code-dir command-not-found history-substring-search vi-mode web-search z autojump)
 
 if is_linux; then
   plugins+=chucknorris
