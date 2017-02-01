da848e46fc21b31af553f425d6e34a8eaf12a507
Stephane Liu <sliu@sjliu.com>
2017-02-01 11:46:24 -1000

    Fix variable name typo

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,7 +1,7 @@
 is_linux() { [ "$(uname -s)" = Linux ] }
 
 if is_linux; then
-  export DLYD_LIBRARY_PATH=~/lib/x86_64-linux-gnu
+  export LD_LIBRARY_PATH=~/lib/x86_64-linux-gnu
   export DYLD_LIBRARY_PATH=/usr/local/lib:$DLYD_LIBRARY_PATH
 fi
 
