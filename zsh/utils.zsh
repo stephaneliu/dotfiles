83a9e0bfb9d972948d9c5adccbebc540d4484404
Stephane Liu <sliu@sjliu.com>
2017-02-01 12:24:38 -1000

    Create zsh for uvscan

--- zsh/utils.zsh
+++ zsh/utils.zsh
@@ -1,9 +1,3 @@
-# Display result of last AV scan
-if [[ -e "/usr/local/uvscan/report.sh" ]]
-then
-  /usr/local/uvscan/report.sh
-fi
-
 if [[ -e "$HOME/bin/jre1.7.0_45" ]]
 then
   export JAVA_HOME=$HOME/bin/jre1.7.0_45
---
+++ zsh/uvscan.zsh
@@ -0,0 +1,6 @@
+# Display result of last AV scan
+if [[ -e "/usr/local/uvscan/report.sh" ]]
+then
+  /usr/local/uvscan/report.sh
+fi
+
