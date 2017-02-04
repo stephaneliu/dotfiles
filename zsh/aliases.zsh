6c97eca6ee8aa33b05e62f57b1830fa814260b0c
Stephane Liu <sliu@sjliu.com>
2017-02-03 21:44:45 -1000

    Add heroku shortcut

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,27 +11,26 @@ alias 'ps?'='ps aux | g '
 alias l='ls -alFHh'
 alias ll='ls -CFHh'
 
+alias 'q;'='exit'
 alias code="echo use 'j'"
 alias d1="echo use 'j'"
-alias w2="echo use 'j'"
-alias w3="echo use 'j'"
-alias w4="echo use 'j'"
-alias screencast='ffmpeg -f x11grab -r 25 -s 1600x1200 -i :0.0 /tmp/output.mpg'
-alias version='cat /etc/issue'
 alias gg='clear'
-alias ssh='TERM=xterm ssh'
-alias mr='mysql -u root'
-
-alias which="which -a"
 # Use modern regexps for grep show color when `grep` is the final command
 # but not when piping into something else because the added color codes will
 # mess up the expected input
 alias grep="egrep --color=auto"
-
+alias h='heroku'
+alias mr='mysql -u root'
 alias nc='nocorrect'
-alias 'q;'='exit'
-alias t='exit'
 alias q='exit'
+alias screencast='ffmpeg -f x11grab -r 25 -s 1600x1200 -i :0.0 /tmp/output.mpg'
+alias ssh='TERM=xterm ssh'
+alias t='exit'
+alias version='cat /etc/issue'
+alias w2="echo use 'j'"
+alias w3="echo use 'j'"
+alias w4="echo use 'j'"
+alias which="which -a"
 
 alias netr='nmcli networking off && nmcli networking on && sleep 10 && ifconfig | grep inet'
 alias inet='ifconfig | grep inet'
