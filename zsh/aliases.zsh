2f4e55a9befba1c347bab75d0e5026a37f7693d1
Stephane Liu <sliu@sjliu.com>
2017-02-10 11:09:44 -1000

    Clarify usage of jump

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -12,8 +12,8 @@ alias l='ls -alFHh'
 alias ll='ls -CFHh'
 
 alias 'q;'='exit'
-alias code="echo use 'j'"
-alias d1="echo use 'j'"
+alias code="echo use 'j'-jump"
+alias d1="echo use 'j'-jump"
 alias gg='clear'
 # Use modern regexps for grep show color when `grep` is the final command
 # but not when piping into something else because the added color codes will
@@ -27,9 +27,9 @@ alias screencast='ffmpeg -f x11grab -r 25 -s 1600x1200 -i :0.0 /tmp/output.mpg'
 alias ssh='TERM=xterm ssh'
 alias t='exit'
 alias version='cat /etc/issue'
-alias w2="echo use 'j'"
-alias w3="echo use 'j'"
-alias w4="echo use 'j'"
+alias w2="echo use 'j'-jump"
+alias w3="echo use 'j'-jump"
+alias w4="echo use 'j'-jump"
 alias which="which -a"
 
 alias netr='nmcli networking off && nmcli networking on && sleep 10 && ifconfig | grep inet'
