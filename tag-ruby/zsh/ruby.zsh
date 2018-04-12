f4875aff1083b7427d8213125e6b81f69e104feb
Stephane Liu <sliu@sjliu.com>
2018-04-11 16:47:20 -1000

    Tweak git and ruby shortcuts

--- tag-ruby/zsh/ruby.zsh
+++ tag-ruby/zsh/ruby.zsh
@@ -2,28 +2,39 @@ alias ann="be annotate -mi -p before"
 alias b='bundle'
 alias be='bundle exec'
 alias bec='bundle exec rails c'
-alias beg='spring stop && bundle exec guard --clear'
-alias ber='bundle exec rake'
-alias bes='bundle exec rails s'
+alias beg="dev_run 'spring stop && bundle exec guard --clear'"
 alias bo='bundle open'
 alias bs='bundle show'
 alias bu='bundle update'
 alias r="rails"
-alias rc="rails c"
-alias rc="rails c"
-alias rg="rails g"
+alias rc="dev_run 'rails c'"
+alias rg="dev_run 'rails g'"
 alias rs="rails server"
 alias ss="spring stop"
-alias tg="be thin -C config/thin.yml start && less log/development.log"
-alias ts="be thin -C config/thin.yml stop"
+
+dev_run() {
+  if dockered; then
+    docker-compose run rails $1
+  else
+    eval $1
+  fi
+}
+
+dockered() {
+  if [ -f ${PWD}/Dockerfile ]; then
+    return 0
+  else
+    return 1
+  fi
+}
 
 # added by travis gem
-[ -f /Users/sliu/.travis/travis.sh ] && source /Users/sliu/.travis/travis.sh
+[ -f ${HOME}/.travis/travis.sh ] && source ${HOME}/.travis/travis.sh
 
-source /usr/local/share/chruby/chruby.sh
-chruby ruby
-
-source $HOME/bin/ruby_switcher.sh
+if [ -f /usr/local/share/chruby/chruby.sh ]; then
+  source /usr/local/share/chruby/chruby.sh
+  chruby ruby
+fi
 
 LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
 if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
@@ -31,3 +42,5 @@ if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
 else
   echo 'Lunchy not installed - gem install lunchy'
 fi
+
+source $HOME/bin/ruby_switcher.sh
