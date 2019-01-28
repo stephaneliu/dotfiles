efccd46b4cfee432fda4e022322481f47878c108
Stephane Liu <sliu@sjliu.com>
2019-01-28 11:38:51 -1000

    Fix: Explicit chruby on zsh load

--- zsh/before/oh_my.zsh
+++ zsh/before/oh_my.zsh
@@ -6,6 +6,6 @@ ZSH_THEME="powerlevel9k/powerlevel9k"
 COMPLETION_WAITING_DOTS="true"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
-plugins=(colored-man-pages command-not-found docker docker-compose heroku history-substring-search osx vi-mode web-search z)
+plugins=(colored-man-pages command-not-found docker docker-compose heroku history-substring-search osx rails vi-mode web-search z)
 
 source $ZSH/oh-my-zsh.sh
