5767a7d35c82f5f204c536b0d10a40ef25b6b3a1
Stephane Liu <sliu@sjliu.com>
2020-03-18 15:10:52 -1000

    Feat: Add vsc in zsh

--- zsh/powerlevel9k.zsh
+++ zsh/powerlevel9k.zsh
@@ -3,7 +3,7 @@ source $HOME/.dotfiles/bin/os_type.sh
 if is_osx; then
   POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(chruby vi_mode vcs newline dir status)
 else
-  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(chruby vi_mode newline dir status)
+  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(chruby vi_mode vcs newline dir status)
 fi
 POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(time)
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
