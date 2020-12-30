dbee6da138c9cb9e97a0b49fcaa13d75094237df
Stephane Liu <sliu@sjliu.com>
2020-12-29 21:33:15 -1000

    Chore: Git and cam mods
      - Remove debug from git hook
      - Assign default cam as fall back

--- zsh/after/find_cam.zsh
+++ zsh/after/find_cam.zsh
@@ -1,3 +1,4 @@
-webcam=$(system_profiler SPCameraDataType | grep Live\ Streamer | sed 's/://' | sed 's/    //') || "FaceTime HD Camera"
+findcam=$(system_profiler SPCameraDataType | grep Live\ Streamer | sed 's/://' | sed 's/    //')
+webcam="${findcam:-'FaceTime HD Camera'}"
 export WEBCAM=$webcam
 export LOLCOMMITS_DEVICE=$webcam
