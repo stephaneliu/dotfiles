276e251de9427255fa9dd9b6d339de1189ae6069
Stephane Liu <sliu@sjliu.com>
2020-12-29 21:57:32 -1000

    Chore: Add cam delay if built in

--- zsh/after/find_cam.zsh
+++ zsh/after/find_cam.zsh
@@ -1,4 +1,10 @@
-findcam=$(system_profiler SPCameraDataType | grep Live\ Streamer | sed 's/://' | sed 's/    //')
-webcam="${findcam:-FaceTime HD Camera (Built-in)}"
+webcam=$(system_profiler SPCameraDataType | grep Live\ Streamer | sed 's/://' | sed 's/    //')
+delay=0
+
+if [ -z "$webcam" ]; then
+  webcam="FaceTime HD Camera (Built-in)"
+  delay=2
+fi
 export WEBCAM=$webcam
+export LOL_DELAY=$delay
 export LOLCOMMITS_DEVICE=$webcam
