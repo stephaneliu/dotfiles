f56645e8d5ff8fc7d85005f639eb6665b63294f4
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2021-11-15 12:51:07 -1000

    Fix: Lolcommit poor image captures
      - Add a 1 second delay

--- zsh/after/find_cam.zsh
+++ zsh/after/find_cam.zsh
@@ -1,11 +1,10 @@
 # webcam=$(system_profiler SPCameraDataType | grep Live\ Streamer | sed 's/://' | sed 's/    //')
 webcam=$(system_profiler SPCameraDataType | grep Logitech\ Webcam\ C930e | sed 's/://' | sed 's/    //')
 
-delay=0
+delay=1
 
 if [ -z "$webcam" ]; then
   webcam="FaceTime HD Camera (Built-in)"
-  delay=2
 fi
 export WEBCAM=$webcam
 export LOL_DELAY=$delay
