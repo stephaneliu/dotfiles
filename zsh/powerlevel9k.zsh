8e5f68db85dfa9c7cbe5c291f44505b88e8e0338
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2021-11-17 16:44:44 -1000

    Add node version to powerlevel9k prompt
      - Remove unnecessary config after time

--- zsh/powerlevel9k.zsh
+++ zsh/powerlevel9k.zsh
@@ -1,21 +1,28 @@
 source $HOME/.dotfiles/bin/os_type.sh
 
-frum_ruby() {
-  local version=$(ruby -v | cut -d ' ' -f2 | cut -d 'p' -f1)
-  echo -n "\ue21e $version"
+ruby_version() {
+  local ruby_version=$(ruby -v | cut -d ' ' -f2 | cut -d 'p' -f1)
+  echo -n "\ue21e $ruby_version"
 }
 
-POWERLEVEL9K_CUSTOM_FRUM_RUBY="frum_ruby"
-POWERLEVEL9K_CUSTOM_FRUM_RUBY_FOREGROUND="black"
-POWERLEVEL9K_CUSTOM_FRUM_RUBY_BACKGROUND="red"
+node_version() {
+  local node_version=$(node -v | cut -d 'v' -f2)
+  echo -n "\u2B21 $node_version"
+}
+
+POWERLEVEL9K_CUSTOM_RUBY_VERSION="ruby_version"
+POWERLEVEL9K_CUSTOM_RUBY_VERSION_FOREGROUND="black"
+POWERLEVEL9K_CUSTOM_RUBY_VERSION_BACKGROUND="red"
+
+POWERLEVEL9K_CUSTOM_NODE_VERSION="node_version"
+POWERLEVEL9K_CUSTOM_NODE_VERSION_FOREGROUND="black"
+POWERLEVEL9K_CUSTOM_NODE_VERSION_BACKGROUND="green"
 
 if is_osx; then
-  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_frum_ruby vi_mode vcs newline dir status)
+  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_ruby_version vi_mode vcs newline dir status)
 else
-  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_frum_ruby vi_mode vcs newline dir status)
+  POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(custom_ruby_version vi_mode vcs newline dir status)
 fi
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(time)
+
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(time custom_node_version)
 POWERLEVEL9K_PROMPT_ADD_NEWLINE=true
-POWERLEVEL9K_CUSTOM_RUBY="echo -n '\ue21e'"
-POWERLEVEL9K_CUSTOM_RUBY_FOREGROUND="black"
-POWERLEVEL9K_CUSTOM_RUBY_BACKGROUND="red"
