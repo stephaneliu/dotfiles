267dc27b75953c55f420670814813cc32cf3a542
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2022-05-12 09:30:58 -1000

    Move find_cam into a function
      - Improve access to find_cam functionality

--- zsh/after/find_cam.zsh
+++ zsh/after/find_cam.zsh
@@ -1,11 +1,19 @@
-# webcam=$(system_profiler SPCameraDataType | grep Live\ Streamer | sed 's/://' | sed 's/    //')
-webcam=$(system_profiler SPCameraDataType | grep Logitech\ Webcam\ C930e | sed 's/://' | sed 's/    //')
+function find_cam {
+  # webcam=$(system_profiler SPCameraDataType | grep Live\ Streamer | sed 's/://' | sed 's/    //')
+  webcam=$(system_profiler SPCameraDataType | grep Logitech\ Webcam\ C930e | sed 's/://' | sed 's/    //')
 
-delay=1
+  delay=1
 
-if [ -z "$webcam" ]; then
-  webcam="FaceTime HD Camera"
-fi
-export WEBCAM=$webcam
-export LOL_DELAY=$delay
-export LOLCOMMITS_DEVICE=$webcam
+  if [ -z "$webcam" ]; then
+    webcam="FaceTime HD Camera"
+  fi
+  export WEBCAM=$webcam
+  export LOL_DELAY=$delay
+  export LOLCOMMITS_DEVICE=$webcam
+
+  if [ $# -eq 0 ]; then
+    echo "Webcam set to $WEBCAM"
+  fi
+}
+
+find_cam 1
