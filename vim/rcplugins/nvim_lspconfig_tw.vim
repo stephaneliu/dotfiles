49f5bc296cb28821388bc5b590a17eec55864a90
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2022-06-20 12:32:17 -1000

    Add TailwindCSS languange server for neovim LSP
      - Add yarn glboal bin https://www.jennapederson.com/blog/2019/10/20/using-yarn-global-packages-with-asdf/

---
+++ vim/rcplugins/nvim_lspconfig_tw.vim
@@ -0,0 +1,3 @@
+lua << EOF
+require('lspconfig').tailwindcss.setup{}
+EOF
--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,2 +1,3 @@
 export PATH=$HOME/bin:$(brew --prefix)/opt/libpq/bin:$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH
+export PATH=$PATH:$HOME/.yarn/bin
 unset LDFLAGS
