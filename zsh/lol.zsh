1ea28125b0693106550803d4cbca94dd7d7b7946
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-02-20 10:10:46 -1000

    LOL - Change dir

--- zsh/lol.zsh
+++ zsh/lol.zsh
@@ -1,4 +1,4 @@
-alias lol='LOLCOMMITS_DIR=$HOME/Documents/lolcommits lolcommits'
-alias lolc='LOLCOMMITS_DIR=$HOME/Documents/lolcommits lolcommits --capture --fork --delay "$LOL_DELAY" --device "$WEBCAM"'
-alias loll='LOLCOMMITS_DIR=$HOME/Documents/lolcommits lolcommits --last'
-alias lolt='LOLCOMMITS_DIR=$HOME/Documents/lolcommits lolcommits --timelapse'
+alias lol='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits'
+alias lolc='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits --capture --fork --delay "$LOL_DELAY" --device "$WEBCAM"'
+alias loll='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits --last'
+alias lolt='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits --timelapse'
