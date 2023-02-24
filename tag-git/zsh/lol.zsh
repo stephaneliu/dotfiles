df7171b389cfa0362a5e9e0770ba236f2ef34041
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-02-24 09:47:34 -1000

    Chore: Assign variable for lol home dir

--- tag-git/zsh/lol.zsh
+++ tag-git/zsh/lol.zsh
@@ -1,4 +1,5 @@
-alias lol='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits'
-alias lolc='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits --capture --fork --delay "$LOL_DELAY" --device "$WEBCAM"'
-alias loll='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits --last'
-alias lolt='LOLCOMMITS_DIR=$HOME/Documents/Resource/lolcommits lolcommits --timelapse'
+lolcommits_home=$HOME/Documents/Resource/lolcommits
+alias lol='LOLCOMMITS_DIR=$lolcommits_home lolcommits'
+alias lolc='LOLCOMMITS_DIR=$lolcommits_home lolcommits --capture --fork --delay "$LOL_DELAY" --device "$WEBCAM"'
+alias loll='LOLCOMMITS_DIR=$lolcommits_home lolcommits --last'
+alias lolt='LOLCOMMITS_DIR=$lolcommits_home lolcommits --timelapse'
