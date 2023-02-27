ac7924f6229c5e62f7a492de83f2891587102641
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-02-27 13:48:06 -1000

    Kitty: alias to change theme
      - kitd - Solarized Dark then reload
      - kitl - Solarized Light then reload

---
+++ zsh/kitty.zsh
@@ -0,0 +1,2 @@
+alias kitd="kitty +kitten themes --dump-theme Solarized Dark > ~/.config/kitty/current-theme.conf; echo 'Dark theme loaded. Please reload \"R\"'"
+alias kitl="kitty +kitten themes --dump-theme Solarized Light > ~/.config/kitty/current-theme.conf; echo 'Light theme loaded. Please reload \"R\"'"
