7a52c2d42c30e356604028712febdb9495705b7d
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-02-27 16:04:59 -1000

    Kitty: Remove zsh alias to change bg color
      - Replaced with ctrl+a>ctrl+l (light) and ctrl+a>ctrl+d (dark)

--- zsh/kitty.zsh
+++
@@ -1,2 +0,0 @@
-alias kitd="kitty +kitten themes --dump-theme Solarized Dark > ~/.config/kitty/current-theme.conf; echo 'Dark theme loaded. Please reload \"R\"'"
-alias kitl="kitty +kitten themes --dump-theme Solarized Light > ~/.config/kitty/current-theme.conf; echo 'Light theme loaded. Please reload \"R\"'"
