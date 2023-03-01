f49eb647bcff1cdeda6ef83eca89146eaf3ba40c
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-03-01 09:51:13 -1000

    kitty: alias for kitty pairing and solo mode

--- zsh/kitty.zsh
+++ zsh/kitty.zsh
@@ -1,1 +1,3 @@
 alias icat="kitty +kitten icat"
+alias pair="kitty @ set-colors --all ~/.config ~/.config/kitty/themes/solarized-light.conf && kitty @ set-font-size 22"
+alias solo="kitty @ set-colors --all ~/.config ~/.config/kitty/themes/solarized-dark.conf && kitty @ set-font-size 13"
