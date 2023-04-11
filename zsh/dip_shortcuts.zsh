d9da0ecce059bb22a92319dc99ab60863653d396
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-04-11 12:50:47 -1000

    dip: Run existing rails c container if exists

--- zsh/dip_shortcuts.zsh
+++ zsh/dip_shortcuts.zsh
@@ -1,7 +1,16 @@
+function drc() {
+  local rc_id=$(docker ps --format "{{.ID}}-{{.Command}}" | grep rails\ c | cut -d '-' -f 1)
+
+  if [ "$(docker ps -a -q -f id=$rc_id)" ]; then
+    docker exec -it $rc_id bin/rails c
+  else
+    dip rails c
+  fi
+}
+
 alias dsh="dip sh"
 alias dg="dip guard"
 alias dr="dip rails"
-alias drc="dip rails c"
 alias drs="dip compose up -d sidekiq webpacker && dip rails s"
 alias ddown="dip compose down"
 alias dbe="dip bundle exec"
