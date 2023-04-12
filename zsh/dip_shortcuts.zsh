8ae9bb554c601125140759037a5397e39be875f4
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-04-11 14:10:27 -1000

    dip: Add option to restart

--- zsh/dip_shortcuts.zsh
+++ zsh/dip_shortcuts.zsh
@@ -1,8 +1,15 @@
 function drc() {
+  local restart=0 && [[ "$1" == "-r" ]] && restart=1
+
   local rc_id=$(docker ps --format "{{.ID}}-{{.Command}}" | grep rails\ c | cut -d '-' -f 1)
 
   if [ "$(docker ps -a -q -f id=$rc_id)" ]; then
-    docker exec -it $rc_id bin/rails c
+    if [ $restart -gt 0 ]; then
+      echo "Restarting rails console container"
+      docker stop $rc_id > /dev/null && dip rails c
+    else
+      docker exec -it $rc_id bin/rails c
+    fi
   else
     dip rails c
   fi
