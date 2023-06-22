f5d60ae3f08f96dbb0495a8412bbb9ae13dce86f
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-06-21 21:52:29 -1000

    Install oh-my-zsh before rcup
      - oh-my-zsh replaces .zshrc
      - Config for oh-my-zsh already captured in zsh/before/oh_my_zsh.zsh

--- zsh/before/oh_my.zsh
+++ zsh/before/oh_my.zsh
@@ -1,9 +1,7 @@
 ZSH=$HOME/.oh-my-zsh
 
 POWERLEVEL9K_MODE='nerdfont-complete'
-
 ZSH_THEME="powerlevel10k/powerlevel10k"
-
 COMPLETION_WAITING_DOTS="true"
 
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
