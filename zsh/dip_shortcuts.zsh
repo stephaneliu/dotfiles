b1c87983814aab58ca0d81d744dc88748689c6ac
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2023-10-25 09:46:40 -1000

    Dip: Fix docker ps truncation preventing finding docker process

--- zsh/dip_shortcuts.zsh
+++ zsh/dip_shortcuts.zsh
@@ -1,12 +1,13 @@
 # `dip rails console - defaults to attaching to existing. Option to restart`
 function drc() {
   local restart=0 && [[ "$1" == "-r" ]] && restart=1
+  local rc_id=$(_docker_id rails\ c)
 
-  local rc_id=$(docker ps --format "{{.ID}}-{{.Command}}" | grep rails\ c | cut -d '-' -f 1)
 
   if [ "$(docker ps -a -q -f id=$rc_id)" ]; then
     if [ $restart -gt 0 ]; then
       echo "Restarting rails console container"
+
       docker stop $rc_id > /dev/null && dip rails c
     else
       docker exec -it $rc_id bin/rails c
@@ -19,12 +20,12 @@ function drc() {
 # `dip shell - defaults to attaching to existing. Option to restart`
 function dsh() {
   local restart=0 && [[ "$1" == "-r" ]] && restart=1
-
-  local sh_id=$(docker ps --format "{{.ID}}-{{.Command}}" | grep bin/bash | cut -d '-' -f 1)
+  local sh_id=$(_docker_id bin/bash)
 
   if [ "$(docker ps -a -q -f id=$sh_id)" ]; then
     if [ $restart -gt 0 ]; then
       echo "Restarting shell container"
+
       docker stop $sh_id > /dev/null && dip sh
     else
       docker exec -it $sh_id /bin/bash
@@ -32,6 +33,10 @@ function dsh() {
   else
     dip sh
   fi
+}
+
+function _docker_id() {
+  docker ps --no-trunc --format "{{.ID}}-{{.Command}}" | grep $1 | cut -d '-' -f 1
 }
 
 alias dip='dip CLEAN_LOGS=1 '
