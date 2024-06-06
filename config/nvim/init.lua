2fa41589cc505dd3f894d2c2b761b6246603d88e
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-06 12:42:02 -1000

    lua: Complete shortcuts

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,20 +1,29 @@
-i_keymap = function(key, val, no_remap)
-  custom_keymap('i', key, val, no_remap)
+__map = function(key, val, no_remap)
+  -- bare 'map'
+  custom_map('', key, val, no_remap)
 end
 
-n_keymap = function(key, val, no_remap)
-  custom_keymap('n', key, val, no_remap)
+c_map = function(key, val, no_remap)
+  custom_map('c', key, val, no_remap)
 end
 
-v_keymap = function(key, val, no_remap)
-  custom_keymap('v', key, val, no_remap)
+i_map = function(key, val, no_remap)
+  custom_map('i', key, val, no_remap)
 end
 
-nv_keymap = function(key, val, no_remap)
-  custom_keymap({'n', 'v'}, key, val, no_remap)
+n_map = function(key, val, no_remap)
+  custom_map('n', key, val, no_remap)
 end
 
-custom_keymap = function(map_type, key, val, no_remap)
+v_map = function(key, val, no_remap)
+  custom_map('v', key, val, no_remap)
+end
+
+nv_map = function(key, val, no_remap)
+  custom_map({'n', 'v'}, key, val, no_remap)
+end
+
+custom_map = function(map_type, key, val, no_remap)
   no_remap = no_remap or true
   vim.keymap.set(map_type, key, val, {noremap = true})
 end
--- config/nvim/lua/options/shortcuts.lua
+++ config/nvim/lua/options/shortcuts.lua
@@ -1,65 +1,29 @@
--- remap ESC in insert mode to jk/kj
-i_keymap('jk', '<ESC>')
-
--- Visual mode select then shift + d to paste selected text
-v_keymap('D', 'y\'>p')
-
--- change working directory to that of file
--- 'c' bindings, abbreviations, currently doesn't work in Lua
--- vim.keymap.set('c', 'cwd', 'lcd %:p:h', {noremap = true})
+i_map('jk', '<ESC>') -- remap ESC in insert mode to jk/kj
+v_map('D', 'y\'>p') -- Visual mode select then shift + d to paste selected text
+c_map('cwd', 'lcd %:p:h') -- change working directory to that of file
+n_map(';v', ':tabnew ~/.config/nvim/init.lua<CR>') -- " Display ~/.vimrc in new tab
+n_map('<leader>rn', ':echo "use unimpared `yor` instead"<CR>') -- " relative and absolute rulers
+nv_map('<leader>hash', ':s/:\\([^ ]*\\)\\(\\s*\\)=>/\\1:/g<CR>') -- convert hash rockets to json style
+n_map('<C-=>', ':resize +3<CR>') -- current window split bigger
+n_map('<C-->', ':resize -3<CR>') -- current window split smaller
+n_map('<Space>', ':nohlsearch<Bar>:echo<CR>') -- Press Space to turn off highlighting and clear any message already displayed
+c_map('w!!', '%!sudo tee > /dev/null') -- vice `sudo !!` from terminal
+__map('<leader>bg', ':echo "use unimpaired `yob` instead"<CR>')
+n_map('<leader>hl', ':echo "use unimpaired `yoc` instead"<CR>') -- toggle current line highlighting
+c_map('fff', "f\\(des\\|con\\|it\\)<CR>") -- Find focus `:fff`
+n_map('gp', "`[v`]`") -- reselect pasted text: https://vimtricks.com/p/reselect-pasted-text/
 
 -- close window with q
-n_keymap(';q', ':q<CR>')
-n_keymap(';Q', ':q!<CR>')
-n_keymap('<leader>t', ':quit<CR>')
-
--- " Display ~/.vimrc in new tab
-n_keymap(';v', ':tabnew ~/.config/nvim/init.lua<CR>')
-
--- " relative and absolute rulers
-n_keymap('<leader>rn', ':echo "use unimpared#yor"<CR>')
-
--- convert hash rockets to json style
-nv_keymap('<leader>hash', ':s/:\\([^ ]*\\)\\(\\s*\\)=>/\\1:/g<CR>')
+n_map(';q', ':q<CR>')
+n_map(';Q', ':q!<CR>')
+n_map('<leader>t', ':quit<CR>')
 
 -- simplify window navigation with ctrl
-n_keymap('<C-j>', '<C-w>j')
-n_keymap('<C-k>', '<C-w>k')
-n_keymap('<C-l>', '<C-w>l')
-n_keymap('<C-h>', '<C-w>h')
+n_map('<C-j>', '<C-w>j')
+n_map('<C-k>', '<C-w>k')
+n_map('<C-l>', '<C-w>l')
+n_map('<C-h>', '<C-w>h')
 
 -- simplify tabbed navigation with shift
-n_keymap('<S-H>', 'gT')
-n_keymap('<S-L>', 'gt')
-
--- current window split bigger
-n_keymap('<C-=>', ':resize +3<CR>')
--- current window split smaller
-n_keymap('<C-->', ':resize -3<CR>')
-
--- remove trailing whitespace and tabs
--- noremap <silent> <F4> :let _s=@/<Bar>:%s/\s\+$//e<Bar>:let @/=_s<Bar>:nohl<CR>:retab<CR>
--- noremap <F5> :buffers<CR>:buffer<Space>
-
-
--- Press Space to turn off highlighting and clear any message already displayed
-n_keymap('<Space>', ':nohlsearch<Bar>:echo<CR>')
-
--- vice sudo !!
--- cmap w!! %!sudo tee > /dev/null %
-
--- toggle background color
--- Use unimpaired - yob
--- map <silent> <leader>bg :let &background = ( &background == "dark"? "light" : "dark" )<CR>
-
--- toggle current line highlighting
--- Use unimpaired - yoc
--- nmap <leader>hl :set cursorline!<CR>
-
--- vim.api.nvim_set_keymap('c', 'fff', "f\\(des\\\\|con\\\\|it\\)<CR>", {noremap = true})
--- vim.cmd[[cnoreabbrev fff f\(des\\|con\\|it\)<CR>]]
--- vim.cmd[[cnoreabbrev fff finding]]
--- vim.keymap.set("ca", "fff", "f\\(des\\\\|con\\\\|it\\)<CR>", {noremap = true})
--- cmap fff f\(des\\|con\\|it\)<CR>
-
--- nnoremap gp `[v`]` " reselect pasted text: https://vimtricks.com/p/reselect-pasted-text/
+n_map('<S-H>', 'gT')
+n_map('<S-L>', 'gt')
