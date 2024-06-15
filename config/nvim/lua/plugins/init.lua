11c2ba8a77cda82a41df7ede085602f2faca3a16
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-15 00:00:24 -1000

    lua: Configure rails plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -4,8 +4,10 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 --
 Plug('rking/ag.vim')
+Plug('tpope/vim-rails')
 --
 vim.call('plug#end')
 
 require("plugins.ag")
+require("plugins.rails")
 -- require("plugins.")
