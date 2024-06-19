10539489c1eca5518102241cd0fa09a9121d5a61
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-18 23:11:48 -1000

    lua: lazy vim conflicted plugin

--- tag-git/config/nvim/lua/plugins/conflicted.lua
+++ tag-git/config/nvim/lua/plugins/conflicted.lua
@@ -1,7 +1,10 @@
--- vim.o.statusline = vim.o.statusline .. "%{ConflictedVersion()}"
---
--- -- Resolve and move to next conflicted file
--- n_map("]m", ":GitNextConflict<cr>")
---
--- vim.g.diffget_local_map = 'gl'
--- vim.g.diffget_upstream_map = 'gu'
+return {
+  'christoomey/vim-conflicted',
+  init = function()
+    vim.o.statusline = vim.o.statusline .. "%{ConflictedVersion()}"
+    -- Resolve and move to next conflicted file
+    n_map("]m", ":GitNextConflict<cr>")
+    vim.g.diffget_local_map = 'gl'
+    vim.g.diffget_upstream_map = 'gu'
+  end
+}
