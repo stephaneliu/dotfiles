ec7e49eca7c689746b4576a99eb7a204ef0dbd7d
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-18 23:15:33 -1000

    lua: lazy vim gh line plugin

--- tag-git/config/nvim/lua/plugins/gh_line.lua
+++ tag-git/config/nvim/lua/plugins/gh_line.lua
@@ -1,5 +1,10 @@
--- -- <leader>gh blob view
--- -- <leader>gb blame view
--- -- <leader>go repo view
---
--- vim.g.gh_use_cononical = 1
+return {
+  -- A Vim plugin that opens a link to the current line on GitHub
+  -- <leader>gh blob view
+  -- <leader>gb blame view
+  -- <leader>go repo view
+  'ruanyl/vim-gh-line',        
+  init = function()
+    vim.g.gh_use_cononical = 1
+  end
+}
