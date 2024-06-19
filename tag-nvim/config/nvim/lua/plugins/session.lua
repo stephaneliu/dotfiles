a92fcaaf082aacca832fdea6e23fc0c88a98624d
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-18 22:05:21 -1000

    lua: lazy-nvim session plugin

--- tag-nvim/config/nvim/lua/plugins/session.lua
+++ tag-nvim/config/nvim/lua/plugins/session.lua
@@ -1,3 +1,10 @@
--- vim.g.session_directory = "~/.config/nvim/sessions"
--- vim.g.session_autosave = 'no'
--- vim.g.session_autoload = 'no'
+-- Manage sessions :SaveSession / OpenSession
+return {
+  'xolox/vim-session',
+  dependencies = { 'xolox/vim-misc' },
+  init = function()
+    vim.g.session_directory = "~/.config/nvim/sessions"
+    vim.g.session_autosave = 'no'
+    vim.g.session_autoload = 'no'
+  end
+}
