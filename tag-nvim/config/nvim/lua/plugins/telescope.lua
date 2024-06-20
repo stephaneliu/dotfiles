4f4b3d7f9f4d3f8a7ac39be51915075b3f860282
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-19 17:30:03 -1000

    lua: Lazy vim telescope
      - 💁 @shaungarwood

---
+++ tag-nvim/config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+
+    telescope.setup({
+      defaults = {
+        path_display = { "truncate " }
+      },
+    })
+
+    telescope.load_extension("fzf")
+  end,
+  keys = {
+    { "<leader>ff", "<CMD>Telescope find_files<CR>", desc = "Fuzzy find files in cwd" },
+    { "<leader>fw", "<CMD>Telescope live_grep<CR>", desc = "Find string in cwd" },
+    { "<leader>fr", "<CMD>Telescope oldfiles<CR>", desc = "Fuzzy find recent files" },
+    { "<leader>fc", "<CMD>Telescope grep_string<CR>", desc = "Find string under cursor in cwd" },
+    { "<leader>fn", "<CMD>Telescope notify<CR>", desc = "Find notifications" },
+  }
+}
