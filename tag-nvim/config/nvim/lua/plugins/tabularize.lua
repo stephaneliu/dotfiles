06c1e9a004c322b33f6f1e647d834bbd67791aab
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-19 16:33:20 -1000

    lua: Lazy vim tabularize plugin

---
+++ tag-nvim/config/nvim/lua/plugins/tabularize.lua
@@ -0,0 +1,15 @@
+return {
+  'godlygeek/tabular',
+  keys = {
+    { '<leader>a=', '<CMD>Tabularize /=<CR><ESC>', mode = {'v'} },
+    { '<leader>ae', '<CMD>Tabularize /=<CR><ESC>', mode = {'v'} },
+    { '<leader>a:', '<CMD>Tabularize /:<CR><ESC>', mode = {'v'} },
+    { '<leader>a;', '<CMD>Tabularize /;<CR><ESC>', mode = {'v'} },
+    { '<leader>a{', '<CMD>Tabularize /{<CR><ESC>', mode = {'v'} }, -- Match only the first { from text selected
+    { '<leader>ac', '<CMD>Tabularize /{<CR><ESC>', mode = {'v'} },
+    { '<leader>ah', '<CMD>Tabularize /=><CR><ESC>', mode = {'v'} },
+    { '<leader>a#', '<CMD>Tabularize /#<CR><ESC>', mode = {'v'} },
+    { '<leader>a-', '<CMD>Tabularize /-<CR><ESC>', mode = {'v'} },
+    { '<leader>as', '<CMD>Tabularize /-><CR><ESC>', mode = {'v'} }, -- match stabby lambda
+  }
+}
