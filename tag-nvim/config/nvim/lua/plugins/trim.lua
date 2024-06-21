ef73de9dcf66bfa905e5283c1ad5510e2980afaf
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-21 12:48:01 -1000

    nvim: Add trim (white space) plugin

---
+++ tag-nvim/config/nvim/lua/plugins/trim.lua
@@ -0,0 +1,20 @@
+-- :TrimToggle topggle trim on save
+-- :Trim trim right now!
+return {
+  'cappyzawa/trim.nvim',
+  opts = {
+    -- you can specify filetypes to ignore.
+    -- ft_blocklist = {"markdown"},
+
+    -- -- if you want to remove multiple blank lines
+    -- patterns = {
+    --   [[%s/\(\n\n\)\n\+/\1/]],   -- replace multiple blank lines with a single line
+    -- },
+    
+    -- if you want to disable trim on write by default
+    trim_on_write = true,
+    
+    -- highlight trailing spaces
+    highlight = true,
+  }
+}
