387ad50e82fea99175c8f94adb2dd49744559de6
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-21 12:25:45 -1000

    nvim: Replace camelcasemotion with spider

--- tag-nvim/config/nvim/lua/plugins/camelcase_motion.lua
+++
@@ -1,8 +0,0 @@
-return {
-  'vim-scripts/camelcasemotion',
-  keys = {
-    { "w", "<Plug>CamelCaseMotion_w" },
-    { "b", "<Plug>CamelCaseMotion_b" },
-    { "e", "<Plug>CamelCaseMotion_e" }
-  }
-}
