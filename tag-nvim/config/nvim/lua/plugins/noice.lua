198fbd049fd30c0a5fda7c24de98a1704fd97b50
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-06-22 11:29:18 -1000

    noice: Decrease message verbosity

--- tag-nvim/config/nvim/lua/plugins/noice.lua
+++ tag-nvim/config/nvim/lua/plugins/noice.lua
@@ -3,25 +3,31 @@ return {
   enabled = true,
   event = "VeryLazy",
   opts = {
-    routes = {
-      {
-        filter = {
-          event = "msg_show",
-          kind = "",
-          find = "written",
-        },
-        opts = { skip = true },
+    lsp = {
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
       },
+      },
+      routes = {
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+      bottom_search = true,
+      command_palette = true,
+      long_message_to_split = true,
     },
-    -- add any options here
-  lsp = {
-    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-    override = {
-      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-      ["vim.lsp.util.stylize_markdown"] = true,
-      ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-    },
-  },
   },
   dependencies = {
     -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
