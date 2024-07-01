82660da48eb8a1ecb91fd04a7e9fab3eca48221b
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-07-01 09:52:22 -1000

    nvim: Todo for color changes

--- tag-nvim/config/nvim/lua/plugins/solarized.lua
+++ tag-nvim/config/nvim/lua/plugins/solarized.lua
@@ -5,6 +5,8 @@ return {
   init = function()
     vim.cmd('syntax enable')
     vim.o.termguicolors = true
+    -- When I change background using vim shortcut it should set an enviornmental variable that can be
+    -- read here when applying the background
     vim.o.background = 'dark'
     vim.cmd([[colorscheme NeoSolarized]])
   end
