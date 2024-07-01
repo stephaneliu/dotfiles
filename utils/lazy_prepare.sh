f9cd52664a6076f91e48bf7d3ef20ec63012d807
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-07-01 09:07:30 -1000

    scripts: Back up and restore lazy and non lazy nvim files

--- utils/lazy_prepare.sh
+++ utils/lazy_prepare.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
 echo "Backing up nvim files"
-mv ~/.local/share/nvim ~/.local/share/nvim.bak
-mv ~/.local/state/nvim ~/.local/state/nvim.bak
-mv ~/.cache/nvim ~/.cache/nvim.bak
+mv ~/.local/share/nvim ~/.local/share/nvim.bak_non_lazy
+mv ~/.local/state/nvim ~/.local/state/nvim.bak_non_lazy
+mv ~/.cache/nvim ~/.cache/nvim.bak_non_lazy
+
+echo "Restoring lazy nvim files"
+mv ~/.local/share/nvim.bak_lazy ~/.local/share/nvim
+mv ~/.local/state/nvim.bak_lazy ~/.local/state/nvim
+mv ~/.cache/nvim.bak_lazy ~/.cache/nvim
 
 echo "Removing nvim configuration files"
 rm -rf ~/.config/nvim
--- utils/lazy_restore.sh
+++ utils/lazy_restore.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
+echo "Backing up lazy nvim files"
+mv ~/.local/share/nvim ~/.local/share/nvim.bak_lazy
+mv ~/.local/state/nvim ~/.local/state/nvim.bak_lazy
+mv ~/.cache/nvim ~/.cache/nvim.bak_lazy
+
 echo "Restoring backup nvim files"
-mv ~/.local/share/nvim.bak ~/.local/share/nvim
-mv ~/.local/state/nvim.bak ~/.local/state/nvim
-mv ~/.cache/nvim.bak ~/.cache/nvim
+mv ~/.local/share/nvim.bak_non_lazy ~/.local/share/nvim
+mv ~/.local/state/nvim.bak_non_lazy ~/.local/state/nvim
+mv ~/.cache/nvim.bak_non_lazy ~/.cache/nvim
 
 echo "Removing nvim configuration files"
 rm -rf ~/.config/nvim
