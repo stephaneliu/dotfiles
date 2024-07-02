a943ef764e073f4c1755d63bd4cbe08fae2f387b
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-07-02 08:37:46 -1000

    nvim: Configure standardrb

--- tag-nvim/config/nvim/lua/config/autocmds.lua
+++ tag-nvim/config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,15 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+
+--- StandardRb LSP for Ruby files
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "ruby",
+  group = vim.api.nvim_create_augroup("RubyLSP", { clear = true }),
+  callback = function()
+    vim.lsp.start({
+      name = "standard",
+      cmd = { "standardrb", "--lsp" },
+      on_attach = function() end,
+    })
+  end,
+})
