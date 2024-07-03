4fd581b5fdb705ab9ac027d56d648e7d982d0b42
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-07-03 12:45:49 -1000

    zsh: Remove invalid options after fzf upgrade

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -7,7 +7,22 @@ fi
 # Key bindings
 source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
 
-export FZF_DEFAULT_OPTS="--no-mouse --height 50% -1 --reverse --multi --inline-info --preview '[[ \$(file --mime {}) =~ binary ]] & echo {} is a binary file || (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300' --preview-window=right:hidden:wrap' --bind 'ctrl-o:execute(bat --style=numbers {} || less -f {}), ctrl+p:toggle-preview,ctrl-d:half-page-down,ctrl-u:half-page-up,ctrl-f:page-down,ctrl-b:page-up,ctrl-y:execute-silent(*echo {+} | pbcopy)'"
+export FZF_DEFAULT_OPTS=" \
+    --no-mouse \
+    --height 50% -1 \
+    --reverse \
+    --multi \
+    --inline-info \
+    --preview '[[ \$(file --mime {}) =~ binary ]] & echo {} is a binary file || \
+        (bat --style=numbers --color=always {} || cat {}) 2> /dev/null | head -300'
+    --preview-window='right:hidden:wrap'
+    --bind 'ctrl-o:execute(bat --style=numbers {} || less -f {})'"
+        # ctrl+p:toggle-preview,\
+        # ctrl-d:half-page-down,\
+        # ctrl-u:half-page-up,\
+        # ctrl-f:page-down,\
+        # ctrl-b:page-up,\
+        # ctrl-y:execute-silent(*echo {+} | pbcopy)'
 
 # export FZF_DEFAULT_COMMAND="git ls-files --cached --others --exclude-standard | fd --type f --type l $FD_OPTIONS"
 # export FZF_CTRL_T_COMMAND="fd $FD_OPTIONS"
