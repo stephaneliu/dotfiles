b1e40bd7a66390583a1a26685a37e7ac78c1bc89
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-07-30 09:41:47 -1000

    zsh: Tweak powerline icons

--- p10k.zsh
+++ p10k.zsh
@@ -99,11 +99,11 @@
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
-    time                    # current time
+    # time                    # current time
     # =========================[ Line #2 ]=========================
-    newline                 # \n
-    # ip                    # ip address and bandwidth usage for a specified network interface
-    public_ip             # public IP address
+    # newline                 # \n
+    ip                    # ip address and bandwidth usage for a specified network interface
+    # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
     # battery               # internal battery
     wifi                  # wifi speed
