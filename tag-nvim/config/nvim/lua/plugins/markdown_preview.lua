b332f185d7ff414bbbdf2a2915e151a150418439
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-07-30 09:29:37 -1000

    nvim: Add markdown preview (,mp) plugin

---
+++ tag-nvim/config/nvim/lua/plugins/markdown_preview.lua
@@ -0,0 +1,19 @@
+return {
+  "iamcco/markdown-preview.nvim",
+  enabled = true,
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = function()
+    vim.fn["mkdp#util#install"]()
+  end,
+  keys = {
+    {
+      "<leader>mp",
+      ft = "markdown",
+      "<cmd>MarkdownPreviewToggle<cr>",
+      desc = "Markdown Preview",
+    },
+  },
+  config = function()
+    vim.cmd([[do FileType]])
+  end,
+}
