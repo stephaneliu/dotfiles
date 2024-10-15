1447d3700c0597414d3bf6e157c23aa26cbc2587
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-10-15 09:21:20 -1000

    Prefill search with word under cursor not working

--- tag-nvim/config/nvim/lua/plugins/grug_far.lua
+++ tag-nvim/config/nvim/lua/plugins/grug_far.lua
@@ -10,5 +10,19 @@ return {
   end,
   keys = {
     { "\\", "<cmd>GrugFar<CR>", { noremap = true } },
+    {
+      "{",
+      function()
+        local grug = require('grug-far')
+        grug.open({
+          prefills = {
+            -- search = "word"
+            search = vim.fn.expand("<cword>")
+          }
+        })
+      end,
+      mode = { "n", "v" },
+      desc = "Search for word under cursor",
+    },
   },
 }
