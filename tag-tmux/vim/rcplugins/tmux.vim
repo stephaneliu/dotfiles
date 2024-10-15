5075a3a4d281817f1974a570a7e78deb19c54cb4
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-10-15 13:24:38 -1000

    tmux: Remove remaining config

--- tag-tmux/vim/rcplugins/tmux.vim
+++
@@ -1,6 +0,0 @@
-" Allow Ctrl+PgUp/PgDn in tmux vs. capitalize word
- set t_kN=[6;*~
- set t_kP=[5;*~
-if !has('nvim')
-  set ttymouse=xterm2
-endif
--- tag-tmux/zsh/tmux.zsh
+++
@@ -1,6 +0,0 @@
-# Prevent tmux sessions from renaming pane title
-export DISABLE_AUTO_TITLE=true
-export AUTO_TITLE=false
-export TERM=xterm-256color
-
-alias mux=tmuxinator
