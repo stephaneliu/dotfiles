c148eb7edce41b2776a9c1bd6733f946554c809a
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-12-13 09:26:54 -1000

    nvim navigator: Enable navigating when split is zoomed

--- config/nvim/lua/plugins/kitty-navigator.lua
+++ config/nvim/lua/plugins/kitty-navigator.lua
@@ -1,4 +1,9 @@
 return {
-    "knubie/vim-kitty-navigator",
-    build = "cp ./*.py ~/.config/kitty/",
+  "knubie/vim-kitty-navigator",
+  build = "cp ./*.py ~/.config/kitty/",
+  init = function()
+    vim.cmd [[
+      let g:kitty_navigator_enable_stack_layout = 1
+    ]]
+  end
 }
