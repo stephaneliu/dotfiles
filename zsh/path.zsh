5ed2a6585998460dff87fd5929bf89a28a63451c
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2024-12-13 17:36:38 -1000

    zsh: Add homebrew paths to point gem install for building

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,3 +1,6 @@
 export PATH=$HOME/bin:$(brew --prefix)/opt/libpq/bin:$(brew --prefix)/bin:$(brew --prefix)/sbin:$PATH
 export PATH=$PATH:$HOME/.yarn/bin
 unset LDFLAGS
+# Homebrew paths to build ruby gems natively
+export LIBRARY_PATH=$LIBRARY_PATH:/opt/homebrew/lib/
+export CPATH=$CPATH:/opt/homebrew/include/
