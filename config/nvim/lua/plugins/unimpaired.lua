cb2eda0d638e5567a816a943792c4ee926be29fd
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2025-09-02 10:55:35 -1000

    nvim: Default displaying relative number in gutter

--- config/nvim/lua/plugins/unimpaired.lua
+++ config/nvim/lua/plugins/unimpaired.lua
@@ -1,4 +1,13 @@
 return {
   'tpope/vim-unimpaired',
   enabled = true,
+  -- Display relative line numbers
+  init = function()
+    vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter", "BufRead", "BufNewFile"}, {
+      pattern = "*",
+      callback = function()
+        vim.wo.relativenumber = true
+      end
+    })
+  end
 }
