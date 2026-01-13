8e12ca20623b1a1d0ea755d78acc697e9924a90e
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-01-13 13:16:44 -1000

    nvim: :SS to save session named [current dir]-[git branch]

--- config/nvim/lua/plugins/session.lua
+++ config/nvim/lua/plugins/session.lua
@@ -8,5 +8,16 @@ return {
     vim.g.session_directory = "/Users/" .. os.getenv("USER") .. "/.config/nvim/sessions"
     vim.g.session_autosave = 'no'
     vim.g.session_autoload = 'no'
+
+    vim.api.nvim_create_user_command('SS', function()
+      local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD 2>/dev/null'):gsub('\n', '')
+      if branch == '' then
+        print('Not in a git repository')
+        return
+      end
+      local dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':t'):gsub('^%.', '')
+      local session_name = dir .. '-' .. branch
+      vim.cmd('SaveSession ' .. session_name)
+    end, {})
   end
 }
