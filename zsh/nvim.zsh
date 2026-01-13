e0af80d59b09c2a050402ed45574f570ede5962c
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-01-13 13:10:43 -1000

    zsh: nvim shortcut vv to open current directory git branch session

--- zsh/nvim.zsh
+++ zsh/nvim.zsh
@@ -1,2 +1,25 @@
 alias vim="echo \"Use v\""
 alias v='nvim +OpenSession'
+
+# Open nvim with session named after current directory and git branch
+function vv() {
+  local branch=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
+  if [[ -z "$branch" ]]; then
+    echo "Not in a git repository"
+    return 1
+  fi
+
+  local dir=$(basename "$PWD")
+  dir="${dir#.}"
+  local session_name="${dir}-${branch}"
+  echo "Opening session: $session_name"
+  local session_file="$HOME/.config/nvim/sessions/${session_name}.vim"
+  if [[ -f "$session_file" ]]; then
+    nvim "+OpenSession $session_name"
+  else
+    echo "Session $session_name not found. Use :SaveSession $session_name to create."
+    read "?Press Enter to open nvim..."
+    local first_file=$(ls -1p | grep -v '/' | head -1)
+    nvim "$first_file"
+  fi
+}
