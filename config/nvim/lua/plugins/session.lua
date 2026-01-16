c9aeac812495a4f3e3974a7b9ffd8a4402baf4ef
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-01-16 10:21:52 -1000

    nvim: session - :SD to delete session

--- config/nvim/lua/plugins/session.lua
+++ config/nvim/lua/plugins/session.lua
@@ -19,5 +19,16 @@ return {
       local session_name = dir .. '-' .. branch
       vim.cmd('SaveSession ' .. session_name)
     end, {})
+
+    vim.api.nvim_create_user_command('SD', function()
+      local branch = vim.fn.system('git rev-parse --abbrev-ref HEAD 2>/dev/null'):gsub('\n', '')
+      if branch == '' then
+        print('Not in a git repository')
+        return
+      end
+      local dir = vim.fn.fnamemodify(vim.fn.getcwd(), ':t'):gsub('^%.', '')
+      local session_name = dir .. '-' .. branch
+      vim.cmd('DeleteSession ' .. session_name)
+    end, {})
   end
 }
