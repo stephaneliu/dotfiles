f89a72d25cf9f19be45bd8d16edb6b1bdccdf405
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-01-27 21:39:42 -1000

    nvim: conflicted - try to fix error when rebase conflict occurs

--- config/nvim/lua/plugins/conflicted.lua
+++ config/nvim/lua/plugins/conflicted.lua
@@ -1,11 +1,22 @@
 return {
   'christoomey/vim-conflicted',
   enabled = true,
+  dependencies = { 'tpope/vim-fugitive' },
+  cmd = { 'Conflicted', 'GitNextConflict' },
   init = function()
-    vim.o.statusline = vim.o.statusline .. "%{ConflictedVersion()}"
-    -- Resolve and move to next conflicted file
-    n_map("]m", ":GitNextConflict<cr>")
     vim.g.diffget_local_map = 'gl'
     vim.g.diffget_upstream_map = 'gu'
+  end,
+  config = function()
+    vim.o.statusline = vim.o.statusline .. "%{ConflictedVersion()}"
+    n_map("]m", ":GitNextConflict<cr>")
+
+    -- Suppress guitablabel error in terminal nvim
+    local orig_conflicted = vim.fn.exists(':Conflicted') == 2 and vim.cmd.Conflicted
+    vim.api.nvim_create_user_command('Conflicted', function()
+      pcall(function()
+        vim.cmd('silent! call conflicted#Conflicted()')
+      end)
+    end, { force = true })
   end
 }
