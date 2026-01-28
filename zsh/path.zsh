dd0943640393608c0bb7262799f98d18a20eb641
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-01-27 21:43:19 -1000

    zsh: Add .local/bin to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -6,3 +6,4 @@ export LIBRARY_PATH=$LIBRARY_PATH:/opt/homebrew/lib/
 export CPATH=$CPATH:/opt/homebrew/include/
 # git-quick-stats requires GNU date in path
 export PATH="$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin:$PATH"
+export PATH="$HOME/.local/bin:$PATH"
