c08d62dac7726bd699291283ed13e990afacc143
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-02-21 17:46:42 -1000

    nvim: cmp - Cycle through popup with <C-n> next and <C-p> previous

---
+++ config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,25 @@
+return {
+  'hrsh7th/nvim-cmp',
+  event = 'InsertEnter',
+  dependencies = {
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+  },
+  config = function()
+    local cmp = require('cmp')
+    cmp.setup({
+      mapping = cmp.mapping.preset.insert({
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+        ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+      }),
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },
+        { name = 'buffer' },
+        { name = 'path' },
+      }),
+    })
+  end,
+}
