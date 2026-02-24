9bbd76adcb5e7080185ff7c65bf899aa770d6ee5
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-02-23 21:22:05 -1000

    nvim: conflicted - Fix git mergetool not displaying files

--- config/nvim/lua/plugins/conflicted.lua
+++ config/nvim/lua/plugins/conflicted.lua
@@ -10,13 +10,5 @@ return {
   config = function()
     vim.o.statusline = vim.o.statusline .. "%{ConflictedVersion()}"
     n_map("]m", ":GitNextConflict<cr>")
-
-    -- Suppress guitablabel error in terminal nvim
-    local orig_conflicted = vim.fn.exists(':Conflicted') == 2 and vim.cmd.Conflicted
-    vim.api.nvim_create_user_command('Conflicted', function()
-      pcall(function()
-        vim.cmd('silent! call conflicted#Conflicted()')
-      end)
-    end, { force = true })
   end
 }
