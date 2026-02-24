f1937ac83aa47c80a3e01b3c12226135a03795ed
Stephane Liu <64043+stephaneliu@users.noreply.github.com>
2026-02-23 17:00:57 -1000

    nvim: neogit - style neogit console (git push) to same theme as nvim

--- config/nvim/lua/plugins/neogit.lua
+++ config/nvim/lua/plugins/neogit.lua
@@ -12,9 +12,28 @@ return {
     { '<leader>gh', '<CMD>DiffviewFileHistory %<CR>', desc = 'File history' },
   },
   config = function()
+    local function set_neogit_highlights()
+      -- Get background from Normal or NormalFloat (handles transparent themes)
+      local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
+      local normal_float = vim.api.nvim_get_hl(0, { name = 'NormalFloat' })
+      local bg = normal.bg or normal_float.bg
+      local fg = normal.fg or normal_float.fg
+
+      if bg then
+        vim.api.nvim_set_hl(0, 'NeogitNormal', { bg = bg, fg = fg })
+        vim.api.nvim_set_hl(0, 'NeogitNormalFloat', { bg = bg, fg = fg })
+      else
+        vim.api.nvim_set_hl(0, 'NeogitNormal', { link = 'NormalFloat' })
+        vim.api.nvim_set_hl(0, 'NeogitNormalFloat', { link = 'NormalFloat' })
+      end
+      vim.api.nvim_set_hl(0, 'NeogitFloatBorder', { link = 'FloatBorder' })
+      vim.api.nvim_set_hl(0, 'NeogitWinSeparator', { link = 'WinSeparator' })
+    end
+
     require('neogit').setup({
       verbose = true,
       kind = 'split',
+      auto_close_console = false,
       integrations = {
         diffview = true,
         telescope = true,
@@ -29,6 +48,26 @@ return {
           end,
         },
       },
+    })
+
+    -- Set highlights initially and on colorscheme change
+    set_neogit_highlights()
+    local augroup = vim.api.nvim_create_augroup('NeogitColors', { clear = true })
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = augroup,
+      callback = set_neogit_highlights,
+    })
+
+    -- Fix terminal buffer background for git console
+    vim.api.nvim_create_autocmd('BufWinEnter', {
+      group = augroup,
+      callback = function()
+        if vim.bo.filetype == 'NeogitConsole' then
+          vim.schedule(function()
+            vim.wo.winhighlight = 'Normal:NeogitNormal,EndOfBuffer:NeogitNormal'
+          end)
+        end
+      end,
     })
   end,
 }
